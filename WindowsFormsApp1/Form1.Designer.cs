11891c7fbae122bcc11aa76584595c388274e6b8
Sam henaghan <sammyhenaghan@gmail.com>
2021-03-18 17:58:24 +0000

    CHEATS

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Inject = new System.Windows.Forms.Button();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.CheckBox = new System.Windows.Forms.CheckBox();
+            this.PicBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PicBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Inject
+            // 
+            this.Inject.BackColor = System.Drawing.Color.Lime;
+            this.Inject.Location = new System.Drawing.Point(69, 25);
+            this.Inject.Name = "Inject";
+            this.Inject.Size = new System.Drawing.Size(108, 48);
+            this.Inject.TabIndex = 0;
+            this.Inject.Text = "Inject";
+            this.Inject.UseVisualStyleBackColor = false;
+            this.Inject.Click += new System.EventHandler(this.Inject_Click);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(69, 91);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(108, 23);
+            this.progressBar2.TabIndex = 2;
+            this.progressBar2.Click += new System.EventHandler(this.progressBar2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(486, 55);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(58, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Aimbot";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(486, 97);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(159, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Fucking insane aimbot 100p";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // CheckBox
+            // 
+            this.CheckBox.AutoSize = true;
+            this.CheckBox.Location = new System.Drawing.Point(486, 248);
+            this.CheckBox.Name = "CheckBox";
+            this.CheckBox.Size = new System.Drawing.Size(64, 17);
+            this.CheckBox.TabIndex = 5;
+            this.CheckBox.Text = "Anti Aim";
+            this.CheckBox.UseVisualStyleBackColor = true;
+            this.CheckBox.CheckedChanged += new System.EventHandler(this.CheckBox_CheckedChanged);
+            // 
+            // PicBox
+            // 
+            this.PicBox.Image = global::WindowsFormsApp1.Properties.Resources.csog_spin;
+            this.PicBox.Location = new System.Drawing.Point(12, 157);
+            this.PicBox.Name = "PicBox";
+            this.PicBox.Size = new System.Drawing.Size(242, 281);
+            this.PicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.PicBox.TabIndex = 7;
+            this.PicBox.TabStop = false;
+            this.PicBox.Click += new System.EventHandler(this.PictureBox2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PicBox);
+            this.Controls.Add(this.CheckBox);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.Inject);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PicBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Inject;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox CheckBox;
+        private System.Windows.Forms.PictureBox PicBox;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Inject_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Successfully injected!");
+        }
+
+        private void progressBar2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if CheckBox.Checked=true then
+                    PicBox.Visible=true
+                Else
+                Pic.Box.Visible = false
+            end if
+            
+
+        }
+    }
+}
